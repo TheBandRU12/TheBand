74648f359d29473f69f3a3380da43f00988f8314
steinit <steinitoddu@gmail.com>
2012-10-31 21:33:16 +0000

    Fyrsta commit á nýju projecti 2

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -10,9 +10,11 @@ public class HelloWorld {
     public static void main(String[] args) {
         Steini steini = new Steini();
         Gudni gudni = new Gudni();
+        Doddi doddi = new Doddi();
         System.out.println("Hæ kæri hópur og velkomin á nýtt github verkefni :-)");
         System.out.println(steini.hallo);
         System.out.println(gudni.gaman);
+        System.out.println(doddi.name);
 
     }
 }
