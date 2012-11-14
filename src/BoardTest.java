549fbf3ad91d5796f0aea4d6ae46841f3fcc5b93
Elin Gylfadottir <egyl14@gmail.com>
2012-11-14 23:36:54 +0000

    Full happy tests with single coord

--- src/BoardTest.java
+++ src/BoardTest.java
@@ -15,18 +15,24 @@ public class BoardTest extends TestCase {
         assertTrue(board.Empty(1));
         board.board[0] = 'X';
         assertFalse(board.Empty(1));
+        board.board[2] = 'O';
+        assertFalse(board.Empty(3));
+        assertTrue(board.Empty(9));
+        board.board[8] = 'X';
+        assertFalse(board.Empty(9));
     }
 
     public void testPlaceMark() throws Exception {
         Board board = new Board();
         assertTrue(board.PlaceMark(1,'X'));
-        //assertFalse(board.PlaceMark(1,'X'));
-        //assertEquals('X', board.GetMark(1));
+        assertEquals('X', board.board[0]);
+        assertFalse(board.PlaceMark(1,'X'));
+        assertEquals('X', board.GetMark(1));
     }
 
     public void testGetMark() throws Exception {
         Board board = new Board();
         board.board[0] = 'X';
-        //assertEquals('X', board.GetMark(1));
+        assertEquals('X', board.GetMark(1));
     }
 }
